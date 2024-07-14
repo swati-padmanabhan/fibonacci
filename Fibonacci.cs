ac09e470aa571672863866a7ac721d1367e2de38
Swati P <67189695+swati-padmanabhan@users.noreply.github.com>
2024-07-14 18:47:31 +0530

    initial commit of Factorial code

---
+++ Fibonacci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Fibonacci
+{
+    public static void Main(string[] args)
+    {
+        int firstNumber = 0;
+        int secondNumber = 1;
+        int result;
+        Console.WriteLine("The Fibonacci Series till are: ");
+        Console.WriteLine(firstNumber + "\n" + secondNumber);
+        for (int i = 0; i <= 10; i++)
+        {
+            result = firstNumber + secondNumber;
+            Console.WriteLine(result);
+            firstNumber = secondNumber;
+            secondNumber = result;
+        }
+
+
+    }
+}
